4e43f4f018d78f322cf83455babc0a23228f20b9
Pradhuman <49186033+gaulghost@users.noreply.github.com>
2020-08-17 00:10:53 +0530

    Add files via upload

--- reset.py
+++ reset.py
@@ -1,1 +1,28 @@
 #Resetting the datsbase to zero.
+import sqlite3
+
+#Carefully delete only when needed, this remove your whole database collected so far
+# conn = sqlite3.connect('dump.sqlite')
+# cur = conn.cursor()
+#
+# cur.executescript('''
+# DROP TABLE IF EXISTS NAMEOFTABLE1;
+# DROP TABLE IF EXISTS NAMEOFTABLE2;
+# ''')
+# conn.commit()
+#
+# cur.close()
+# print("Dump data has been reset")
+
+#Resets the ranks
+conn = sqlite3.connect('sorted.sqlite')
+cur = conn.cursor()
+
+cur.executescript('''
+DROP TABLE IF EXISTS NAMEOFTABLE1;
+DROP TABLE IF EXISTS NAMEOFTABLE2;
+''')
+conn.commit()
+
+cur.close()
+print("Sorted data has been reset")
